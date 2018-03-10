1ccebe03a50a959c65071cadd9cda8db000a52bb
Mathalin <36937431+Mathalin@users.noreply.github.com>
2018-03-10 01:25:45 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -10,7 +10,8 @@
 #include <string.h>
 
 long Mote;
-float battery, temper, humid, light, redlig;
+float battery, temper, humid, light, redlig, dtemper, dhumid, dlight;
+float room[13][3]={0};
 
 int sensorread(void){
     char name[100]="12 13 7E 15 16 17 7E 45 00 FF FF 00 05 0A 3F 0C 0F CC 1D 3F 02 27 00 BD 01 6D 48 81 7E", valor[40];
@@ -87,8 +88,48 @@ int sensorread(void){
 	return 1;
 }
 
+int definevalues(void){
+	int option=0, i=0;
+	while(option!=15){
+		printf("\t-Choose the Room-\n \t1 - Gym \n \t2 - Entrance \n \t3 - Office \n \t4 - Nursery \n \t5 - Bathroom 1 \n \t6 - Bathroom 2 \n \t7 - Bathroom 3 \n \t8 - Bathroom 4 \n \t9 - 2/3 age Room \n \t10 - 4/5 age Room \n \t11 - Study Room \n \t12 - Canteen \n \t13 - Kitchen \n --Rooms with no values will not be controled-- \n 14 - See values \n 15 - Exit Menu \n");
+		scanf("%i", &option);
+		if(option<13 && option >0){
+		printf("-Values-\nTemperature (C):");
+		scanf("%f", &room[option][0]);
+		printf("Humidity (%%):");
+		scanf("%f", &room[option][1]);
+		printf("Light (lux):");
+		scanf("%f", &room[option][2]);
+		}else if(option == 14){
+			printf("-Preset Values-\nRoom         Temp  Humi  Light\n");
+			for(i=0; i<13; i++){
+				switch(i+1){
+					case 1: printf("Gym          "); break;
+					case 2: printf("Entrance     "); break;
+					case 3: printf("Office       "); break;
+					case 4: printf("Nursery      "); break;
+					case 5: printf("Bathroom 1   "); break;
+					case 6: printf("Bathroom 2   "); break;
+					case 7: printf("Bathroom 3   "); break;
+					case 8: printf("Bathroom 4   "); break;
+					case 9: printf("2/3 age Room "); break;
+					case 10: printf("4/5 age Room "); break;
+					case 11: printf("Study Room   "); break;
+					case 12: printf("Canteen      "); break;
+					case 13: printf("Kitchen      "); break;
+				}
+				printf("%.2f %.2f %.2f\n", room[i][0], room[i][1], room[i][2]);
+			}
+		}
+	}
+	return 1;
+}
+
 int main(){
-		
+	
+	int pro;
+	pro = definevalues();	
+	printf("%i", pro);
 	
 	return 0;
 }
